010fd7da885743977f759a7ea93d3c1e2c645a01
Vladimir <posvyanskij@gmail.com>
2015-07-15 15:03:19 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,34 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function returns a list of functions. The first one is used when we'd like to change the matrix. 
+#The second function get the initial matrix. The third one, gets the matrix after inversion
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+   x <<- y
+   m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(invert) m <<- invert
+  getinverse <- function() m
+  list(get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
 ## Write a short comment describing this function
+## This function do all the work. If it's first time we use the function it calls the setinverse() from makeCacheMatrix, 
+##than compute the inverse function and save it into m. The value of m is in the environment of cacheSolve
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  mat <- x$get()
+  m <- solve(mat, ...)
+  x$setinverse(m)
+  m
 }
